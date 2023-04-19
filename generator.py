f9922e33cb7b2de6b6bdfbb8c23a5c1d27dda339
“wwoodbery” <wyatt_woodbery@brown.edu>
2023-04-19 12:40:32 -0400

    Generator done

--- generator.py
+++ generator.py
@@ -2,39 +2,82 @@ import tensorflow as tf
 from keras.layers import Dense
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import Conv2D
-from keras.layers import PReLU
+from keras.layers import PReLU, LeakyReLU
+from keras.layers import Input
+from keras.layers import add
+from keras.layers.convolutional import UpSampling2D
+from keras.models import Model
 
-def residual_block(kernel_size, filters, strides):
-    return \
-    Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding="same"), \
-    BatchNormalization(momentum=0.5), \
-    PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2]),\
-    Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding="same"),\
-    BatchNormalization(momentum=0.5)
+
+def residual_block(inputs, kernel_size, filters, strides):
+    og_inputs = inputs
+
+    inputs = Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding="same")(inputs)
+    inputs = BatchNormalization(momentum=0.5)(inputs)
+    inputs = PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2])(inputs)
+    inputs = Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding="same")(inputs)
+    inputs = BatchNormalization(momentum=0.5)(inputs)
+    
+    inputs = add([og_inputs, inputs])
+
+def upsampling_block(inputs, kernel_size, filters, strides):
+    inputs = Conv2D(filters = filters, kernel_size = kernel_size, strides = strides, padding = "same")(inputs)
+    inputs = UpSampling2D(size = 2)(inputs)
+    inputs = LeakyReLU(alpha = 0.2)(inputs)
+
+    return inputs
 
 class Generator(object):
 
+    def __init__(self, input_shape):
+        self.input_shape = input_shape
+
     def generator(self, input):
 
-        model = tf.keras.Sequential([
-            Conv2D(filters=64, kernel_size=9, strides=1, padding="same"),
-            PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2]),
-            residual_block(3,64,1),
-            residual_block(3,64,1),
-            residual_block(3,64,1),
-            residual_block(3,64,1),
-            residual_block(3,64,1),
-            residual_block(3,64,1),
-            residual_block(3,64,1),
-            residual_block(3,64,1),
-            residual_block(3,64,1),
-            residual_block(3,64,1),
-            residual_block(3,64,1),
-            residual_block(3,64,1),
-            residual_block(3,64,1),
-            residual_block(3,64,1),
-            residual_block(3,64,1),
-            residual_block(3,64,1),
-            Conv2D(filters=64, kernel_size=3, strides=1, padding="same"),
-            BatchNormalization(momentum = 0.5)(model)
-        ])
+        ## storing original inputs for use in model declaration later
+        og_inputs = Input(shape = self.input_shape)
+
+        ## first block
+        inputs = Conv2D(filters = 64, kernel_size = 9, strides = 1, padding = "same")(og_inputs)
+        inputs = PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2])(inputs)
+
+        ## storing output of first block for skip connection later
+        first_block_outputs = inputs
+
+        ## 16 residual block
+        inputs = residual_block(inputs, 3, 64, 1)
+        inputs = residual_block(inputs, 3, 64, 1)
+        inputs = residual_block(inputs, 3, 64, 1)
+        inputs = residual_block(inputs, 3, 64, 1)
+        inputs = residual_block(inputs, 3, 64, 1)
+        inputs = residual_block(inputs, 3, 64, 1)
+        inputs = residual_block(inputs, 3, 64, 1)
+        inputs = residual_block(inputs, 3, 64, 1)
+        inputs = residual_block(inputs, 3, 64, 1)
+        inputs = residual_block(inputs, 3, 64, 1)
+        inputs = residual_block(inputs, 3, 64, 1)
+        inputs = residual_block(inputs, 3, 64, 1)
+        inputs = residual_block(inputs, 3, 64, 1)
+        inputs = residual_block(inputs, 3, 64, 1)
+        inputs = residual_block(inputs, 3, 64, 1)
+        inputs = residual_block(inputs, 3, 64, 1)
+        
+        ## skip connection
+        inputs = Conv2D(filters = 64, kernel_size = 3, strides = 1, padding = "same")(inputs)
+        inputs = BatchNormalization(momentum = 0.5)(inputs)
+        inputs = add([first_block_outputs, inputs])
+
+        ## 2 upsampling blocks
+        inputs = upsampling_block(inputs, 3, 256, 1)
+        inputs = upsampling_block(inputs, 3, 256, 1)
+
+        inputs = Conv2D(filters = 3, kernel_size = 9, strides = 1, padding = "same")(inputs)
+        inputs = tf.keras.activations.tanh(inputs)
+
+        ## disambiguate naming
+        model_output = inputs
+
+        return Model(inputs=og_inputs, outputs=model_output)
+
+
+
