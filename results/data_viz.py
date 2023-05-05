8b8056c9323d07786e33b08b2189ee9ad5a97407
indiameicorengold <indiameicorengold@gmail.com>
2023-05-05 01:34:26 -0400

    oops
    need to fix axes

--- results/data_viz.py
+++ results/data_viz.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_all():
+def get_data():
     data = pd.read_csv('results/loss.txt', names=['Epochs', 'd_loss_fake', 'd_loss_real', 'loss_gan', 'loss_gan1', 'loss_gan2'])
+    df = data
+    for col in df:
+        df[col] =df[col].astype(str).str.replace("[","").str.replace("]","")
+    return data
+
+def plot_all(data):
     epochs = data.loc[:, 'Epochs']
     plt.figure(figsize=(20,15))
     plt.plot(epochs, data.loc[:, 'd_loss_fake'], label='d_loss_fake')
@@ -15,11 +21,11 @@ def plot_all():
     plt.ylabel('Loss', size=30)
     plt.legend(fontsize=20)
     plt.savefig('results/graphs/all.png')
+    print(data)
 
-def plot_d_fake():
-    data = pd.read_csv('results/loss.txt', names=['Epochs', 'd_loss_fake', 'd_loss_real', 'loss_gan', 'loss_gan1', 'loss_gan2'])
+def plot_d_fake(data):
     epochs = data.loc[:, 'Epochs']
-    plt.figure(figsize=(15,15))
+    plt.figure(figsize=(20,15))
     plt.plot(epochs, data.loc[:, 'd_loss_fake'], label='d_loss_fake')
     plt.title('Discriminator Loss on Fake Images', size=40)
     plt.xlabel('Epochs', size=30)
@@ -27,10 +33,9 @@ def plot_d_fake():
     plt.legend(fontsize=20)
     plt.savefig('results/graphs/d_loss_fake.png')
 
-def plot_d_real():
-    data = pd.read_csv('results/loss.txt', names=['Epochs', 'd_loss_fake', 'd_loss_real', 'loss_gan', 'loss_gan1', 'loss_gan2'])
+def plot_d_real(data):
     epochs = data.loc[:, 'Epochs']
-    plt.figure(figsize=(15,15))
+    plt.figure(figsize=(20,15))
     plt.plot(epochs, data.loc[:, 'd_loss_real'], label='d_loss_real')
     plt.title('Discriminator Loss on Real Images', size=40)
     plt.xlabel('Epochs', size=30)
@@ -38,8 +43,7 @@ def plot_d_real():
     plt.legend(fontsize=20)
     plt.savefig('results/graphs/d_loss_real.png')
 
-def plot_gen():
-    data = pd.read_csv('results/loss.txt', names=['Epochs', 'd_loss_fake', 'd_loss_real', 'loss_gan', 'loss_gan1', 'loss_gan2'])
+def plot_gen(data):
     epochs = data.loc[:, 'Epochs']
     plt.figure(figsize=(20,15))
     plt.plot(epochs, data.loc[:, 'loss_gan'], label='loss_gan')
@@ -52,7 +56,8 @@ def plot_gen():
     plt.savefig('results/graphs/generator.png')
 
 if __name__ == '__main__':
-    plot_all()
-    plot_d_fake()
-    plot_d_real()
-    plot_gen()
+    data = get_data()
+    plot_all(data)
+    plot_d_fake(data)
+    plot_d_real(data)
+    plot_gen(data)
